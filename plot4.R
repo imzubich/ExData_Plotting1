bbb53f26954d2e012d36abd9d92a7fc9d266577d
Zubair Afzal <zailafzal167@gmail.com>
2025-09-06 12:45:30 +0500

    Final Version

--- plot4.R
+++ plot4.R
@@ -1,3 +1,18 @@
+data <- read.table("household_power_consumption.txt",
+                   header = TRUE,
+                   sep = ";", 
+                   na.strings = "?",
+                   stringsAsFactors = FALSE)
+
+
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+subset_data <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
+
+datetime <- strptime(paste(subset_data$Date, subset_data$Time),
+                     format="%Y-%m-%d %H:%M:%S")
+subset_data$Global_active_power <- as.numeric(subset_data$Global_active_power)
+
+
 png("plot4.png", width = 480, height = 480)
 par(mfrow = c(2,2), mfcol = c(2,2))
 
@@ -19,6 +34,7 @@ plot(datetime, subset_data$Voltage,
 plot(datetime, subset_data$Global_reactive_power,
      type = "l",
      ylab = "Global_reactive_power")
+
 
 
 
